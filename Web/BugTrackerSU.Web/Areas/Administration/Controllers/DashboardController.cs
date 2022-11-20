1cd660066b7fd5342dae967f52cc8fa1e8bbf4c9
Velislav Marinov <101339074+VelislavMarinov@users.noreply.github.com>
2022-11-20 13:26:44 +0200

    Created RolesController and RoleViewModel, added action SetRoleToUser in UserService

--- Web/BugTrackerSU.Web/Areas/Administration/Controllers/DashboardController.cs
+++ Web/BugTrackerSU.Web/Areas/Administration/Controllers/DashboardController.cs
@@ -1,28 +1,12 @@
 ﻿namespace BugTrackerSU.Web.Areas.Administration.Controllers
 {
-    using BugTrackerSU.Services.Data;
-    using BugTrackerSU.Services.Data.User;
-
     using Microsoft.AspNetCore.Mvc;
 
     public class DashboardController : AdministrationController
     {
-        private readonly ISettingsService settingsService;
-
-        private readonly IUserService userService;
-
-        public DashboardController(
-            ISettingsService settingsService,
-            IUserService userService)
+       public IActionResult Index()
         {
-            this.settingsService = settingsService;
-            this.userService = userService;
-        }
-
-        public IActionResult ManageUserRoles()
-        {
-            var model = this.userService.GetAllUsersAndRoles();
-            return this.View(model);
+            return View();
         }
     }
 }
