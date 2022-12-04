5fd49833de5314e44ffd83ccc3432b8acdaf853c
Velislav Marinov <101339074+VelislavMarinov@users.noreply.github.com>
2022-12-04 18:42:18 +0200

    Created method CreateTicketHistoryAsync in TicketHistoryService.

--- Services/BugTrackerSU.Services.Data/TicketHistory/ITicketHistoryService.cs
+++ Services/BugTrackerSU.Services.Data/TicketHistory/ITicketHistoryService.cs
@@ -3,7 +3,11 @@
     using System;
     using System.Threading.Tasks;
 
+    using BugTrackerSU.Data.Models;
+    using BugTrackerSU.Web.ViewModels.Tickets;
+
     public interface ITicketHistoryService
     {
+        Task CreateTicketHistoryAsync(int tikcetId, EditTicketViewModel model);
     }
 }
