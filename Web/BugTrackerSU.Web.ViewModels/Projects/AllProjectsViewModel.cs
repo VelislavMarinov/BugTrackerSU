9b13dd3122364e9929534a7fe38f40587e397e14
Velislav Marinov <101339074+VelislavMarinov@users.noreply.github.com>
2022-12-11 21:56:44 +0200

    Created AllProjectsViewModel and PagingViewModel.

---
+++ Web/BugTrackerSU.Web.ViewModels/Projects/AllProjectsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BugTrackerSU.Web.ViewModels.Projects
+{
+    using System.Collections.Generic;
+
+    public class AllProjectsViewModel : PagingViewModel
+    {
+        public List<ProjectViewModel> Projects { get; set; }
+    }
+}
