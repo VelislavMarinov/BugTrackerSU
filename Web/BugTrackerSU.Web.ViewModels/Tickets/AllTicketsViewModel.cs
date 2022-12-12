2c1f7419a65240eeb38302db95f13f5f5756af89
Velislav Marinov <101339074+VelislavMarinov@users.noreply.github.com>
2022-12-12 12:56:46 +0200

    Created _TicketsPagingPartial, and AllTicketsViewModel.

---
+++ Web/BugTrackerSU.Web.ViewModels/Tickets/AllTicketsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BugTrackerSU.Web.ViewModels.Tickets
+{
+    using System.Collections.Generic;
+
+    public class AllTicketsViewModel : PagingViewModel
+    {
+        public List<TicketViewModel> Tickets { get; set; }
+    }
+}
